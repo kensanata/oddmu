cc58980ec0375b17704aff9769ddd68f77aab625
Alex Schroeder <alex@gnu.org>
2023-09-15 00:50:50 +0200

    Add command line search

--- commands.go
+++ commands.go
@@ -14,12 +14,23 @@ func commands() {
 			p.renderHtml();
 			fmt.Println(p.Html);
 		}
+	} else if len(os.Args) > 2 && os.Args[1] == "search" {
+		loadIndex()
+		for _, q := range os.Args[2:] {
+			items := search(q)
+			fmt.Printf("Search %s: %d results\n", q, len(items))
+			for _, p := range items {
+				fmt.Printf("* %s (%d)\n", p.Title, p.Score)
+			}
+		}
 	} else {
 		fmt.Printf("Unknown command: %v\n", os.Args[1:])
 		fmt.Print("Without any arguments, serves a wiki.\n")
-		fmt.Print(" Environment variable ODDMUSE_PORT controls the port.\n")
-		fmt.Print(" Environment variable ODDMUSE_LANGAUGES controls the languages detected.\n")
+		fmt.Print("  Environment variable ODDMUSE_PORT controls the port.\n")
+		fmt.Print("  Environment variable ODDMUSE_LANGAUGES controls the languages detected.\n")
 		fmt.Print("html PAGENAME\n")
-		fmt.Print(" Print the HTML of the page.\n")
+		fmt.Print("  Print the HTML of the page.\n")
+		fmt.Print("search TERMS\n")
+		fmt.Print("  Print the titles of the page with score.\n")
 	}
 }
