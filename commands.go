ca59a1ae5f329a7408289a195dd02cb6591c89f6
Alex Schroeder <alex@gnu.org>
2023-09-19 14:37:13 +0200

    Add a replace command

--- commands.go
+++ commands.go
@@ -4,8 +4,16 @@ import (
 	"context"
 	"flag"
 	"fmt"
+	"io/fs"
 	"os"
+	"path/filepath"
+	"regexp"
+	"slices"
+	"strings"
 	"github.com/google/subcommands"
+	"github.com/hexops/gotextdiff"
+	"github.com/hexops/gotextdiff/myers"
+	"github.com/hexops/gotextdiff/span"
 )
 
 type htmlCmd struct {
@@ -15,8 +23,8 @@ type htmlCmd struct {
 func (*htmlCmd) Name() string     { return "html" }
 func (*htmlCmd) Synopsis() string { return "Render a page as HTML." }
 func (*htmlCmd) Usage() string {
-  return `html <page name>:
-  Render a page as HTML
+  return `html [-view] <page name>:
+  Render a page as HTML.
 `
 }
 
@@ -60,9 +68,9 @@ func (cmd *searchCmd) SetFlags(f *flag.FlagSet) {
 func (*searchCmd) Name() string     { return "search" }
 func (*searchCmd) Synopsis() string { return "Search pages and print a list of links." }
 func (*searchCmd) Usage() string {
-  return `search <terms>:
+  return `search [-page <n>] <terms>:
   Search for pages matching terms and print the result set as a
-  Markdown list. Before searching, it indexes all the pages. Thus,
+  Markdown list. Before searching, all the pages are indexed. Thus,
   startup is slow. The benefit is that the page order and scores are
   exactly as when the wiki runs.
 `
@@ -87,6 +95,77 @@ func (cmd *searchCmd) Execute(_ context.Context, f *flag.FlagSet, _ ...interface
 	return subcommands.ExitSuccess
 }
 
+type replaceCmd struct {
+	confirm bool
+}
+
+func (cmd *replaceCmd) SetFlags(f *flag.FlagSet) {
+  f.BoolVar(&cmd.confirm, "confirm", false, "do the replacement instead of just doing a dry run")
+}
+
+func (*replaceCmd) Name() string     { return "replace" }
+func (*replaceCmd) Synopsis() string { return "Search and replace a regular expression." }
+func (*replaceCmd) Usage() string {
+  return `replace [-confirm] <regexp> <replacement>:
+  Search a regular expression and replace it. By default, this is a
+  dry run and nothing is saved. The replacement can use $1, $2, etc.
+  to refer to capture groups in the regular expression.
+`
+}
+
+func (cmd *replaceCmd) Execute(_ context.Context, f *flag.FlagSet, _ ...interface{}) subcommands.ExitStatus {
+	args := f.Args()
+	if len(args) != 2 {
+		fmt.Println("Replace takes exactly two arguments.")
+		return subcommands.ExitFailure
+	}
+	re := regexp.MustCompile(args[0])
+	repl := []byte(args[1])
+	changes := 0
+	err := filepath.Walk(".", func(path string, info fs.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if info.IsDir() || strings.HasPrefix(path, ".") || !strings.HasSuffix(path, ".md") {
+			return nil
+		}
+		body, err := os.ReadFile(path)
+		if err != nil {
+			return err
+		}
+		result := re.ReplaceAll(body, repl)
+		if !slices.Equal(result, body) {
+			changes++
+			if !cmd.confirm {
+				edits := myers.ComputeEdits(span.URIFromPath(path + "~"), string(body), string(result))
+				diff := fmt.Sprint(gotextdiff.ToUnified(path + "~", path, string(body), edits))
+				fmt.Println(diff)
+			} else {
+				fmt.Println(path)
+				_ = os.Rename(path, path+"~")
+				err = os.WriteFile(path, result, 0644)
+				if err != nil {
+					return err
+				}
+			}
+		}
+		return nil
+	})
+	if err != nil {
+		fmt.Println(err)
+		return subcommands.ExitFailure
+	}
+	if changes == 1 {
+		fmt.Println("1 change was made.")
+	} else {
+		fmt.Printf("%d changes were made.\n", changes)
+	}
+	if !cmd.confirm && changes > 0 {
+		fmt.Println("This is a dry run. Use -confirm to make it happen.")
+	}
+	return subcommands.ExitSuccess
+}
+
 // commands does the command line parsing in case Oddmu is called with
 // some arguments. Without any arguments, the wiki server is started.
 // At this point we already know that there is at least one
@@ -97,6 +176,7 @@ func commands() {
 	subcommands.Register(subcommands.CommandsCommand(), "")
 	subcommands.Register(&htmlCmd{}, "")
 	subcommands.Register(&searchCmd{}, "")
+	subcommands.Register(&replaceCmd{}, "")
 
 	flag.Parse()
 	ctx := context.Background()
