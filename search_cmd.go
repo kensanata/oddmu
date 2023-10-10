2736e76d07dff5a640314b1831a32c3773a82c1f
Alex Schroeder <alex@gnu.org>
2023-10-10 11:13:44 +0200

    Add optional extracts to the search command

--- search_cmd.go
+++ search_cmd.go
@@ -4,18 +4,22 @@ import (
 	"context"
 	"flag"
 	"fmt"
+	"github.com/charmbracelet/lipgloss"
 	"github.com/google/subcommands"
 	"io"
 	"os"
+	"regexp"
 	"strings"
 )
 
 type searchCmd struct {
 	page int
+	extract bool
 }
 
 func (cmd *searchCmd) SetFlags(f *flag.FlagSet) {
 	f.IntVar(&cmd.page, "page", 1, "the page in the search result set")
+	f.BoolVar(&cmd.extract, "extract", false, "print page extract instead of link list")
 }
 
 func (*searchCmd) Name() string     { return "search" }
@@ -30,12 +34,12 @@ func (*searchCmd) Usage() string {
 }
 
 func (cmd *searchCmd) Execute(_ context.Context, f *flag.FlagSet, _ ...interface{}) subcommands.ExitStatus {
-	return searchCli(os.Stdout, cmd.page, f.Args())
+	return searchCli(os.Stdout, cmd.page, cmd.extract, f.Args())
 }
 
 // searchCli runs the search command on the command line. It is used
 // here with an io.Writer for easy testing.
-func searchCli(w io.Writer, n int, args []string) subcommands.ExitStatus {
+func searchCli(w io.Writer, n int, extract bool, args []string) subcommands.ExitStatus {
 	index.load()
 	q := strings.Join(args, " ")
 	items, more := search(q, ".", n)
@@ -44,8 +48,23 @@ func searchCli(w io.Writer, n int, args []string) subcommands.ExitStatus {
 	} else {
 		fmt.Fprintf(w, "Search for %s, page %d: %d results\n", q, n, len(items))
 	}
-	for _, p := range items {
-		fmt.Fprintf(w, "* [%s](%s)\n", p.Title, p.Name)
+	if extract {
+		heading := lipgloss.NewStyle().Bold(true).Underline(true)
+		quote := lipgloss.NewStyle().PaddingLeft(4).Width(78)
+		match := lipgloss.NewStyle().Bold(true)
+		re := regexp.MustCompile(`<b>(.*?)</b>`)
+		for _, p := range items {
+			s := re.ReplaceAllString(string(p.Html), match.Render(`$1`))
+			fmt.Fprintln(w, heading.Render(p.Title))
+			if p.Name != p.Title {
+				fmt.Fprintln(w, p.Name)
+			}
+			fmt.Fprintln(w, quote.Render(s))
+		}		
+	} else {
+		for _, p := range items {
+			fmt.Fprintf(w, "* [%s](%s)\n", p.Title, p.Name)
+		}
 	}
 	if more {
 		fmt.Fprintf(w, "There are more results\n")
