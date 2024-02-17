30b0b64129e7d0de14a34af631b4e6c8a9e09e0f
Alex Schroeder <alex@gnu.org>
2024-02-17 23:38:25 +0100

    Test whether the main man page links to all the others

---
+++ man_test.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"github.com/stretchr/testify/assert"
+	"io/fs"
+	"path/filepath"
+	"os"
+	"strings"
+	"testing"
+)
+
+func TestManPages(t *testing.T) {
+	b, err := os.ReadFile("man/oddmu.1.txt");
+	main := string(b)
+	assert.NoError(t, err)
+	filepath.Walk("man", func (path string, info fs.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if strings.HasSuffix(path, ".txt") &&
+			path != "man/oddmu.1.txt" {
+			s := strings.TrimPrefix(path, "man/")
+			s = strings.TrimSuffix(s, ".txt")
+			i := strings.LastIndex(s, ".")
+			ref := "_" + s[:i] + "_(" + s[i+1:] + ")"
+			assert.Contains(t, main, ref, ref)
+		}
+		return nil
+	})
+}
+
