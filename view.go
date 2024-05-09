71315bc662ce429691b20f387f160e823fb130e9
Alex Schroeder <alex@gnu.org>
2024-05-09 21:13:05 +0200

    Split previewHandler from view.go to preview.go

--- view.go
+++ view.go
@@ -140,17 +140,3 @@ func viewHandler(w http.ResponseWriter, r *http.Request, path string) {
 	p.renderHtml()
 	renderTemplate(w, p.Dir(), "view", p)
 }
-
-// previewHandler is a bit like saveHandler and viewHandler. Instead of saving the date to a page, we create a synthetic
-// Page and render it. Note that when saving, the carriage returns (\r) are removed. We need to do this as well,
-// otherwise the rendered template has garbage bytes at the end. Note also that we need to remove the title from the
-// page so that the preview works as intended (and much like the "view.html" template) where as the editing requires the
-// page content including the header… which is why it needs to be added in the "preview.html" template. This makes me
-// sad.
-func previewHandler(w http.ResponseWriter, r *http.Request, path string) {
-	body := strings.ReplaceAll(r.FormValue("body"), "\r", "")
-	p := &Page{Name: path, Body: []byte(body)}
-	p.handleTitle(true)
-	p.renderHtml()
-	renderTemplate(w, p.Dir(), "preview", p)
-}
