aae2ae1265f02cc57ed75a10d5138f71b5346c16
Alex Schroeder <alex@gnu.org>
2024-08-21 18:05:09 +0200

    The "html" subcommand takes - for stdin

--- html_cmd.go
+++ html_cmd.go
@@ -18,6 +18,7 @@ func (*htmlCmd) Synopsis() string { return "render a page as HTML" }
 func (*htmlCmd) Usage() string {
 	return `html [-view] <page name> ...:
   Render one or more pages as HTML.
+  Use a single - to read Markdown from stdin.
 `
 }
 
@@ -30,27 +31,44 @@ func (cmd *htmlCmd) Execute(_ context.Context, f *flag.FlagSet, _ ...interface{}
 }
 
 func htmlCli(w io.Writer, useTemplate bool, args []string) subcommands.ExitStatus {
+	if len(args) == 1 && args[0] == "-" {
+		body, err := io.ReadAll(os.Stdin)
+		if err != nil {
+			fmt.Fprintf(w, "Cannot read from stdin: %s\n", err)
+			return subcommands.ExitFailure
+		}
+		p := &Page{Name: "stdin", Body: body}
+		return p.printHtml(w, useTemplate)
+	}
 	for _, arg := range args {
 		p, err := loadPage(arg)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "Cannot load %s: %s\n", arg, err)
 			return subcommands.ExitFailure
 		}
-		if useTemplate {
-			p.handleTitle(true)
-			p.renderHtml()
-			t := "view.html"
-			loadTemplates()
-			err := templates.template[t].Execute(w, p)
-			if err != nil {
-				fmt.Fprintf(os.Stderr, "Cannot execute %s template for %s: %s\n", t, arg, err)
-				return subcommands.ExitFailure
-			}
-		} else {
-			// do not handle title
-			p.renderHtml()
-			fmt.Fprintln(w, p.Html)
+		status := p.printHtml(w, useTemplate)
+		if status != subcommands.ExitSuccess {
+			return status
 		}
+	}
+	return subcommands.ExitSuccess
+}
+
+func (p *Page) printHtml(w io.Writer, useTemplate bool) subcommands.ExitStatus {
+	if useTemplate {
+		t := "view.html"
+		loadTemplates()
+		p.handleTitle(true)
+		p.renderHtml()
+		err := templates.template[t].Execute(w, p)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Cannot execute %s template for %s: %s\n", t, p.Name, err)
+			return subcommands.ExitFailure
+		}
+	} else {
+		// do not handle title
+		p.renderHtml()
+		fmt.Fprintln(w, p.Html)
 	}
 	return subcommands.ExitSuccess
 }
