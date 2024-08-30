609da1fbc23c3f04415df32050970860ccf7cd0f
Alex Schroeder <alex@gnu.org>
2024-08-30 17:52:59 +0200

    Improve the man tests
    Add a check for template documentation. Add checks to prevent tests
    from passing when nothing was found.

--- man_test.go
+++ man_test.go
@@ -19,12 +19,14 @@ func TestManPages(t *testing.T) {
 	b, err := os.ReadFile("man/oddmu.1.txt")
 	main := string(b)
 	assert.NoError(t, err)
+	count := 0
 	filepath.Walk("man", func(path string, info fs.FileInfo, err error) error {
 		if err != nil {
 			return err
 		}
 		if strings.HasSuffix(path, ".txt") &&
 			path != "man/oddmu.1.txt" {
+			count++
 			s := strings.TrimPrefix(path, "man/")
 			s = strings.TrimSuffix(s, ".txt")
 			i := strings.LastIndex(s, ".")
@@ -33,20 +35,45 @@ func TestManPages(t *testing.T) {
 		}
 		return nil
 	})
+	assert.Greater(t, count, 0, "no man pages were found")
+}
+
+// Does oddmu-templates(5) mention all the templates?
+func TestManTemplates(t *testing.T) {
+	b, err := os.ReadFile("man/oddmu-templates.5.txt")
+	man := string(b)
+	assert.NoError(t, err)
+	count := 0
+	filepath.Walk(".", func(path string, info fs.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if strings.HasSuffix(path, ".html") {
+			count++
+			assert.Contains(t, man, path, path)
+		}
+		if path != "." && info.IsDir() {
+			return filepath.SkipDir
+		}
+		return nil
+	})
+	assert.Greater(t, count, 0, "no templates were found")
 }
 
 // Does oddmu(1) mention all the actions? We're not going to parse the go file and make sure to catch them all. I tried
 // it, and it's convoluted.
-func TestManActionsPages(t *testing.T) {
+func TestManActions(t *testing.T) {
 	b, err := os.ReadFile("man/oddmu.1.txt")
 	assert.NoError(t, err)
 	main := string(b)
 	b, err = os.ReadFile("wiki.go")
 	assert.NoError(t, err)
 	wiki := string(b)
+	count := 0
 	// this doesn't match the root handler
 	re := regexp.MustCompile(`http.HandleFunc\("(/[a-z]+/)", makeHandler\([a-z]+Handler, (true|false)\)\)`)
 	for _, match := range re.FindAllStringSubmatch(wiki, -1) {
+		count++
 		var path string
 		if match[2] == "true" {
 			path = "_" + match[1] + "dir/name"
@@ -55,6 +82,7 @@ func TestManActionsPages(t *testing.T) {
 		}
 		assert.Contains(t, main, path, path)
 	}
+	assert.Greater(t, count, 0, "no handlers were found")
 	// root handler is manual
 	assert.Contains(t, main, "\n- _/_", "root")
 }
@@ -65,11 +93,13 @@ func TestReadme(t *testing.T) {
 	b, err := os.ReadFile("README.md")
 	readme := string(b)
 	assert.NoError(t, err)
+	count := 0
 	filepath.Walk("man", func(path string, info fs.FileInfo, err error) error {
 		if err != nil {
 			return err
 		}
 		if strings.HasSuffix(path, ".txt") {
+			count++
 			s := strings.TrimPrefix(path, "man/")
 			s = strings.TrimSuffix(s, ".txt")
 			i := strings.LastIndex(s, ".")
@@ -78,6 +108,8 @@ func TestReadme(t *testing.T) {
 		}
 		return nil
 	})
+	assert.Greater(t, count, 0, "no man pages were found")
+	count = 0
 	filepath.Walk(".", func(path string, info fs.FileInfo, err error) error {
 		if err != nil {
 			return err
@@ -85,12 +117,14 @@ func TestReadme(t *testing.T) {
 		if strings.HasSuffix(path, ".go") &&
 			!strings.HasSuffix(path, "_test.go") &&
 			!strings.HasSuffix(path, "_cmd.go") {
+			count++
 			s := strings.TrimPrefix(path, "./")
 			ref := "`" + s + "`"
 			assert.Contains(t, readme, ref, ref)
 		}
 		return nil
 	})
+	assert.Greater(t, count, 0, "no source pages were found")
 }
 
 // Does the README document all the dependecies, checking all the all the packages with names containing a period?
@@ -112,7 +146,7 @@ func TestDocumentDependencies(t *testing.T) {
 			}
 		}
 	}
-	assert.Greater(t, len(imports), 0, "Imports found")
+	assert.Greater(t, len(imports), 0, "no imports found")
 	sort.Slice(imports, func(i, j int) bool { return len(imports[i]) < len(imports[j]) })
 IMPORT:
 	for _, name := range imports {
