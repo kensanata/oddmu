e975c527d1b6a74a783a4721bf1e38f64731b5cf
Alex Schroeder <alex@gnu.org>
2024-08-30 14:03:32 +0200

    Add test for all actions in the man page
    Add the /list/, /delete/ and /rename/ actions to the man page.

--- man_test.go
+++ man_test.go
@@ -7,12 +7,14 @@ import (
 	"io/fs"
 	"os"
 	"path/filepath"
+	"regexp"
 	"slices"
 	"sort"
 	"strings"
 	"testing"
 )
 
+// Does oddmu(1) link to all the other man pages?
 func TestManPages(t *testing.T) {
 	b, err := os.ReadFile("man/oddmu.1.txt")
 	main := string(b)
@@ -33,9 +35,35 @@ func TestManPages(t *testing.T) {
 	})
 }
 
+// Does oddmu(1) mention all the actions? We're not going to parse the go file and make sure to catch them all. I tried
+// it, and it's convoluted.
+func TestManActionsPages(t *testing.T) {
+	b, err := os.ReadFile("man/oddmu.1.txt")
+	assert.NoError(t, err)
+	main := string(b)
+	b, err = os.ReadFile("wiki.go")
+	assert.NoError(t, err)
+	wiki := string(b)
+	// this doesn't match the root handler
+	re := regexp.MustCompile(`http.HandleFunc\("(/[a-z]+/)", makeHandler\([a-z]+Handler, (true|false)\)\)`)
+	for _, match := range re.FindAllStringSubmatch(wiki, -1) {
+		var path string
+		if match[2] == "true" {
+			path = "_" + match[1] + "dir/name"
+		} else {
+			path = "_" + match[1] + "dir/"
+		}
+		assert.Contains(t, main, path, path)
+	}
+	// root handler is manual
+	assert.Contains(t, main, "\n- _/_", "root")
+}
+
+// Does the README link to all the man pages and all the Go source files,
+// excluding the command and test files?
 func TestReadme(t *testing.T) {
 	b, err := os.ReadFile("README.md")
-	main := string(b)
+	readme := string(b)
 	assert.NoError(t, err)
 	filepath.Walk("man", func(path string, info fs.FileInfo, err error) error {
 		if err != nil {
@@ -46,7 +74,7 @@ func TestReadme(t *testing.T) {
 			s = strings.TrimSuffix(s, ".txt")
 			i := strings.LastIndex(s, ".")
 			ref := "[" + s[:i] + "(" + s[i+1:] + ")]"
-			assert.Contains(t, main, ref, ref)
+			assert.Contains(t, readme, ref, ref)
 		}
 		return nil
 	})
@@ -59,12 +87,13 @@ func TestReadme(t *testing.T) {
 			!strings.HasSuffix(path, "_cmd.go") {
 			s := strings.TrimPrefix(path, "./")
 			ref := "`" + s + "`"
-			assert.Contains(t, main, ref, ref)
+			assert.Contains(t, readme, ref, ref)
 		}
 		return nil
 	})
 }
 
+// Does the README document all the dependecies, checking all the all the packages with names containing a period?
 func TestDocumentDependencies(t *testing.T) {
 	b, err := os.ReadFile("README.md")
 	readme := string(b)
@@ -83,6 +112,7 @@ func TestDocumentDependencies(t *testing.T) {
 			}
 		}
 	}
+	assert.Greater(t, len(imports), 0, "Imports found")
 	sort.Slice(imports, func(i, j int) bool { return len(imports[i]) < len(imports[j]) })
 IMPORT:
 	for _, name := range imports {
